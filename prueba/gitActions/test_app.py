fd905739d6544b8d5f1d4f67b72189d2e2e693a4
Joelo15 <dylanjoelmolina@outlook.com>
2025-10-24 15:42:59 -0500

    primer commit

---
+++ prueba/gitActions/test_app.py
@@ -0,0 +1,11 @@
+import unittest
+import os
+from app import suma
+
+class TestApp(unittest.TestCase):
+    def test_suma(self):
+        self.assertEqual(suma(2, 3), 5)   
+        self.assertEqual(suma(-1, 1), 0)
+
+if __name__ == "__main__":
+    unittest.main()
